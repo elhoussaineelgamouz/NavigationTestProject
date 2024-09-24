2bc44df92b4f1f1e3e5632b90490242a3b260445
elhoussaine elgamouz <elhoussaineelgamouz@gmail.com>
2024-09-24 11:54:44 +0100

    Initial Commit

---
+++ NavigationTestProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationTestProject
+//
+//  Created by El houssaine El GAMOUZ on 24/9/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationTestProject/NavigationTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTestProjectApp.swift
+//  NavigationTestProject
+//
+//  Created by El houssaine El GAMOUZ on 24/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
